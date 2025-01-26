6d12ab1545b64b36be2a2e450b9a78b7ce0a7179
Robin John <johnrobin403@gmail.com>
2025-01-26 08:36:52 +0530

    Fixed CORS 2 hopefully

--- backend/CodeInit/settings.py
+++ backend/CodeInit/settings.py
@@ -132,4 +132,25 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+]
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+)
